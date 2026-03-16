a9523433fa0325b0740118c57845a0222f159b12
kquirapas <kmquirapas@gmail.com>
2026-03-16 11:24:10 +0000

    doc: Document intention

--- app/services/mod.rs
+++ app/services/mod.rs
@@ -1,5 +1,8 @@
 //! Services to call API services and convert
 //! them into HTML for htmx.
+//!
+//! This is where external services are integrated
+//! and where middleware resides.
 pub mod admin;
 pub mod blog;
 pub mod data;
